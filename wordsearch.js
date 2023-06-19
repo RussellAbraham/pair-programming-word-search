97009e7ae47f87630f6390cce31a8a230ff034c4
RussellAbraham <russell_a@live.com>
2023-06-18 21:03:47 -0700

    Fix wordsearch function to pass test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        let column = '';
+        for (let j = 0;j < letters.length; j++) {
+            column += letters[j][i];
+        }
+        verticalJoin.push(column);
     }
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for(l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
 }
 
 module.exports = wordSearch
